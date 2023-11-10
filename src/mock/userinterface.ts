7ad7bdaa6561c45e79f5af3ed74770b6c4c511a6
Aaron Cox <aaron@greymass.com>
2023-11-09 17:01:30 -0800

    Updating to Session 1.1.0

--- src/mock/userinterface.ts
+++ src/mock/userinterface.ts
@@ -10,6 +10,7 @@ import {
     PromptArgs,
     PromptResponse,
     UserInterface,
+    UserInterfaceAccountCreationResponse,
     UserInterfaceLoginResponse,
 } from '@wharfkit/session'
 
@@ -97,5 +98,14 @@ export class MockUserInterface extends AbstractUserInterface implements UserInte
 
     addTranslations(definitions: LocaleDefinitions): void {
         this.log('addTranslations: ' + JSON.stringify(definitions))
+    }
+
+    async onAccountCreate(): Promise<UserInterfaceAccountCreationResponse> {
+        this.log('onAccountCreate')
+        return Promise.resolve({})
+    }
+
+    async onAccountCreateComplete(): Promise<void> {
+        this.log('onAccountCreateComplete')
     }
 }
